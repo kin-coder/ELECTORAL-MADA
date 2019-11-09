3d29d2b1b93f506d3c9f003862e0c4cf52915859
Michael RAKOTOVAO <michael77rakotovao@gmail.com>
2019-11-09 20:22:04 +0300

    add maj candidate

--- BACK/voting_app/app/models/candidate.rb
+++ BACK/voting_app/app/models/candidate.rb
@@ -4,6 +4,11 @@ class Candidate
   field :first_name, type: String
   field :last_name, type: String
   field :avatar, type: String
+  
+  field :fokotany, type: String
+  field :commune, type: String
+  field :district, type: String
+  field :region, type: String
 
   has_many :voters
 end
