749160b3bd6bfdc16a96eee0ec69c4106f447777
Itokiana <itokianarajoelison@gmail.com>
2019-11-10 01:09:30 +0300

    Add candidate manager

---
+++ BACK/voting_app/app/helpers/candidate_manager_helper.rb
@@ -0,0 +1,2 @@
+module CandidateManagerHelper
+end
--- BACK/voting_app/config/routes.rb
+++ BACK/voting_app/config/routes.rb
@@ -5,12 +5,15 @@ Rails.application.routes.draw do
   resources :confirmations, controller: 'rails_jwt_auth/confirmations', only: [:create, :update]
   resources :registration, controller: 'rails_jwt_auth/registrations', only: [:create, :update, :destroy]
   resources :session, controller: 'rails_jwt_auth/sessions', only: [:create, :destroy]
-  # resource :registration, controller: 'registrations', only: [:create, :update, :destroy]
+
   get '/api/voters', to: 'api_voter#all_voters'
   get '/api/voters/:search', to: 'api_voter#search_voters'
-  get '/api/test', to: 'api_voter#voter_test'
-  get '/oauth/authorize/:entitled/:secret', to: 'oauth_application#authorize'
+  # get '/api/test', to: 'api_voter#voter_test'
+  # get '/oauth/authorize/:entitled/:secret', to: 'oauth_application#authorize'
   post '/api/session_vote/begin', to: 'api_vote_manager#begin_session'
   post '/api/session_vote/end', to: 'api_vote_manager#end_session'
+
+  post '/api/candidates', to: 'candidate_manager#all_candidates'
+  post '/api/candidates/general_stat', to: 'candidate_manager#all_vote_details'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
