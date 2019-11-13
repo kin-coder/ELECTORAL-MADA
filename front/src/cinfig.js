92d10d96a65cdbc0de4726bcdbdcf42960ade0fb
tahinarakotoniain <tahinarakotoniain@gmail.com>
2019-11-13 12:05:17 +0300

    congig front by ngrok Itokiaina

--- front/src/cinfig.js
+++ front/src/cinfig.js
@@ -1,6 +1,6 @@
-// prod
-// const BASE_URL = ['https://api.electoral-mada.org'];
+// api enligne
+const BASE_URL = ['https://be2f7a1c.ngrok.io/'];
 
 // dev
-const BASE_URL = ['http://127.0.0.1:3000'];
+// const BASE_URL = ['http://127.0.0.1:3000'];
 export default BASE_URL;
